26b10fcfc65d41515830ff53fd8675e9f0c139ed
eladFrizi <okc.elad.35@gmail.com>
2017-07-28 22:33:50 +0300

    added the projects page

--- server.js
+++ server.js
@@ -56,6 +56,12 @@ app.get('/about', (req, res) => {
     });
 })
 
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {});
+})
+
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMassage: 'Unable to handle request'
